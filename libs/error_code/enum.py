451eadb49c77ceaafc13013692295533ec77834c
liuseki <ld@meng2333.com>
2021-10-12 10:51:38 +0800

    错误码逻辑优化

--- libs/error_code/enum.py
+++ libs/error_code/enum.py
@@ -3,11 +3,11 @@
 """错误码枚举类"""
 
 from __future__ import annotations
+
 from typing import NamedTuple
-from enum import Enum, EnumMeta
+from enum import Enum, EnumMeta, unique
 from types import DynamicClassAttribute
 
-from libs.enum import keyword_value_unique
 
 __all__ = (
     'ECData',
@@ -20,15 +20,20 @@ class ECData(NamedTuple):
     code: str     # 错误码
     message: str  # 错误信息
 
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, ECData):
+            raise NotImplemented
+        return self.code == other.code
+
 
 class _ECEnumMeta(EnumMeta):
 
     def __new__(mcs, *args, **kwargs):
         enum_class = super(_ECEnumMeta, mcs).__new__(mcs, *args, **kwargs)
-        # 校验code唯一
-        enum_class = keyword_value_unique(('code',))(enum_class)
+        # 全部code
         enum_class._member_codes_ = tuple(enum.value.code for enum in enum_class)
-        return enum_class
+        # code唯一
+        return unique(enum_class)
 
     @property
     def codes(cls) -> tuple[str, ...]:
